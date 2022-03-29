be8e6c041b7ec90373b2a6982b8c0a462ea3038b
sidra1991 <1991sidra@gmail.com>
2022-03-29 15:38:59 +0200

    moltiplicazione

--- src/components/mainVision.vue
+++ src/components/mainVision.vue
@@ -1,18 +1,20 @@
 <template>
-    <ul>
-        <li>
-            {{arrCard[0].title}}
-        </li>
-        <li>
-            {{arrCard[0].original_title}}
-        </li>
-        <li>
-            {{arrCard[0].original_language}}
-        </li>
-        <li>
-            {{arrCard[0].vote_average}}
-        </li>
-    </ul>
+    <div>
+        <ul v-for="(el, index) in this.arrCard"  :key="index" >
+            <li>
+                {{el.title}}
+            </li>
+            <li>
+                {{el.original_title}}
+            </li>
+            <li>
+                {{el.original_language}}
+            </li>
+            <li>
+                {{el.vote_average}}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -21,18 +23,18 @@ export default {
     name:'mainVision',
     data (){
         return{
-            arrCard:''
+            arrCard:'',
+            titleMedia: 'naruto'
         }
     },
     created () {
-        axios.get('https://api.themoviedb.org/3/search/movie?api_key=2a1eafb77e5173892c5f55c2d7d7a8c8&query=naruto')
-      .then((response) => {
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=2a1eafb77e5173892c5f55c2d7d7a8c8&query=${this.titleMedia}`)
+        .then((response) => {
         this.arrCard = response.data.results
-        console.log('primo -------', response)
-        console.log('secondo -------', response.data)
-        console.log('terzo-----', response.data.results  )
-        console.log('quarto------', response.data.results[0])
-        // console.log( 'test', this.arrCard[0].vote_average);
+        // console.log('primo -------', response)
+        // console.log('secondo -------', response.data)
+        // console.log('terzo-----', response.data.results  )
+        // console.log('quarto------', response.data.results[0])
     })
   }
 }
