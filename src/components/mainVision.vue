74e5edaec2661bf5eac76cc58181fb6f9856249e
sidra1991 <1991sidra@gmail.com>
2022-03-29 23:02:54 +0200

    immagini aggiunte

--- src/components/mainVision.vue
+++ src/components/mainVision.vue
@@ -2,7 +2,8 @@
     <div>
         <navBar @search="model"/>
         <p>{{prova}}</p>
-        <ul v-for="(el, index) in this.arrCard"  :key="index" >
+        <ul v-for="(el, index) in this.arrCard"  :key="index">
+            <img :src="immage+el.backdrop_path" :alt="el.title">
             <li>
                 {{el.title}}
             </li>
@@ -10,12 +11,31 @@
                 {{el.original_title}}
             </li>
             <li>
+                <img :src="flag+el.original_language+png" alt="">
                 {{el.original_language}}
             </li>
             <li>
                 {{el.vote_average}}
             </li>
         </ul>
+        <p>-------------------------------------------------------------</p>
+        <ul v-for="(el, index) in this.arrCardTv"  :key="index" >
+            <img :src="immage+el.backdrop_path" :alt="el.title">
+            <li>
+                {{el.name}}
+            </li>
+            <li>
+                {{el.original_name}}
+            </li>
+            <li>
+                <img :src="flag+el.original_language+png" alt="">
+                {{el.original_language}}
+            </li>
+            <li>
+                {{el.vote_average}}
+            </li>
+        </ul>
+
     </div>
 </template>
 
@@ -31,8 +51,12 @@ export default {
     name:'mainVision',
     data (){
         return{
+            flag:'https://flagcdn.com/16x12/',
+            png:'.png',
+            immage:'https://image.tmdb.org/t/p/w300/',
             prova:'',
             arrCard:'',
+            arrCardTv:'',
             titleMedia: 'naruto'
         }
     },
@@ -45,6 +69,11 @@ export default {
             .then((response) => {
                 this.arrCard = response.data.results
             })
+            axios.get(`https://api.themoviedb.org/3/search/tv?api_key=2a1eafb77e5173892c5f55c2d7d7a8c8&query=it_IT&query=${this.titleMedia}`)
+            .then((responTV) => {
+                this.arrCardTv = responTV.data.results
+                // console.log('quarto------', responTV.data.results)
+            })
         }
     },
     created () {
@@ -54,7 +83,7 @@ export default {
         // console.log('primo -------', response)
         // console.log('secondo -------', response.data)
         // console.log('terzo-----', response.data.results  )
-        // console.log('quarto------', response.data.results[0])
+        console.log('quarto------', response.data.results[0])
         })
     },
 }
@@ -65,5 +94,4 @@ export default {
 </script>
 
 <style>
-
 </style>
--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -7,7 +7,8 @@
 
 <script>
 export default {
-    name: "navBar"
+  titleFilms:'',
+  name: "navBar"
 };
 </script>
 
