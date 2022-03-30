d6bc80c4d464abf8e7d7260937e04cf3d02b582e
sidra1991 <1991sidra@gmail.com>
2022-03-30 21:24:19 +0200

    creando il layout

--- src/components/cardFilmsOriginal.vue
+++ src/components/cardFilmsOriginal.vue
@@ -1,28 +1,27 @@
 <template>
-  <div>
-      <h2>film</h2>
-      <ul v-for="(el, index) in arrCardFilm"  :key="index">
-        <li>
-          <img :src="immage+el.backdrop_path" :alt="el.title" v-if="el.backdrop_path != null">
-          <div class="ssimmage" v-else></div>
-
-        </li>
-        <li>
-          {{el.title}}
-        </li>
-        <li>
-          {{el.original_title}}
-        </li>
-        <li>
-           <img :src="flag+el.original_language+png" alt="">
-            {{el.original_language}}
-        </li>
-        <li>
-          <span v-for="(star, index) in Math.ceil(el.vote_average /2)" :key="index" > ★ </span>
-          <span v-for="(star, index) in  5 - (Math.ceil(el.vote_average /2))" :key="index" > ☆ </span>
-        </li>
-      </ul>
+<div class="center-card">
+  <div class="container-card">
+      <div class="card"  v-for="(el, index) in arrCardFilm"  :key="index">
+        <img class="bg-img" :src="immage+el.backdrop_path" :alt="el.title" v-if="el.backdrop_path != null">
+        <ul>
+          <li>
+            {{el.title}}
+          </li>
+          <li>
+            {{el.original_title}}
+          </li>
+          <li>
+            <img :src="flag+el.original_language+png" alt="">
+              {{el.original_language}}
+          </li>
+          <li>
+            <span v-for="(star, index) in Math.ceil(el.vote_average /2)" :key="index" > ★ </span>
+            <span v-for="(star, index) in  5 - (Math.ceil(el.vote_average /2))" :key="index" > ☆ </span>
+          </li>
+        </ul>
+      </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -31,7 +30,7 @@ export default {
     return{
       flag:'https://flagcdn.com/16x12/',
       png:'.png',
-      immage:'https://image.tmdb.org/t/p/w300/',
+      immage:'https://image.tmdb.org/t/p/w342/',
     }
   },
   props: {
@@ -41,9 +40,31 @@ export default {
 </script>
 
 <style>
-  .ssimmage {
-    height: 100px;
-    widows: 100px;
-    background: black;
+  .center-card {
+    display: flex;
+    justify-content: center;
   }
+
+  .container-card {
+    display: flex;
+    width: 80%;
+    flex-wrap: wrap;
+  }
+
+  .card{
+    display: inline-block;
+    position: relative;
+    height: 200px;
+    width: 350px;
+    background: black;
+    margin :2rem;
+  }
+
+  .bg-img{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+
+
 </style>
